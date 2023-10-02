137c72169e6bb983178a8b5fa3639b857521f8ed
Evgennna <138823629+Evgennna@users.noreply.github.com>
2023-10-02 22:04:30 +0300

    Update test_two.py

--- test_two.py
+++ test_two.py
@@ -2,21 +2,21 @@ import time
 
 import pytest
 from testpage import OperationHelper
-username = "KiraZ2"
-password = "gi89mjb90f"
+username = "29103740"
+password = "39d8bf048b"
 
 def test_step_1(browser):
     test_page1 = OperationHelper(browser)
     test_page1.go_to_site()
-    test_page1.enter_login("KiraZ2")
-    test_page1.enter_pswd("gi89mjb90f")
+    test_page1.enter_login("29103740")
+    test_page1.enter_pswd("39d8bf048b")
     test_page1.click_button()
     time.sleep(3)
   
     test_page1.click_contact()
     time.sleep(3)
     
-    test_page1.enter_cont_name("Kira")
+    test_page1.enter_cont_name("29103740")
     test_page1.enter_cont_email("mail@mail.ru")
     test_page1.enter_cont_text("Hi, how are you?")
     time.sleep(1)
